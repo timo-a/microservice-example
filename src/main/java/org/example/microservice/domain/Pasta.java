1819a3625d0ad3c563bb7ce75f960a5fac2a1069
timo <1398557+timo-a@users.noreply.github.com>
2024-07-22 21:18:05 +0200

    add 2 domain objects

---
+++ src/main/java/org/example/microservice/domain/Pasta.java
@@ -0,0 +1,4 @@
+package org.example.microservice.domain;
+
+public record Pasta(String name, int price) {
+}
---
+++ src/main/java/org/example/microservice/domain/Pizza.java
@@ -0,0 +1,12 @@
+package org.example.microservice.domain;
+
+import lombok.Value;
+
+@Value
+public class Pizza {
+
+    String name;
+
+    int price;
+}
+
